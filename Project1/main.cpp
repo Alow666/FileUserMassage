05cb9dcc336fa00998caee7201c3507978b05acc
Дионисий <bloodstart14@gmail.com>
2025-02-27 20:08:35 +0700

    27.02.2025 end

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -8,6 +8,17 @@
 namespace fs = std::filesystem;
 
 int main() {
+	std::fstream userfile("all_user.txt", std::ios::in | std::ios::out);
+	std::fstream messagefile("all_message.txt", std::ios::in | std::ios::out);
+
+
+
+
+
+
+
+
+
 
 
 	return 0;
